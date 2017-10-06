111ec3ae2c0b704673ef8d3866fa0c308d4f628a
Wei Ping Chen <waypin2002@gmail.com>
2017-10-06 10:27:50 +0800

    simple example

---
+++ Test1Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Restful.Service;
+
+namespace Restful.Controllers
+{
+    public class Test1Controller : ApiController
+    {
+        CustomStyle custom = new CustomStyle();
+
+        public string getData()
+        {
+            string colorhtml = custom.colorName();
+            string content = "<p>這是由Web Api 發出的資料</p>";
+            string html = string.Format("<div class='alert alert-{0}'>{1}</div>", colorhtml, content);
+            return html;
+        }
+    }
+}
---
+++ Test2Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Restful.Service;
+
+namespace Restful.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        CustomStyle custom = new CustomStyle();
+        // GET: /Test2/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string getdata(string input)
+        {
+            string colorName = custom.colorName();
+            string content = "<p>這是由Controller 發出的資料</p>";
+            string html = string.Format("<div class='alert alert-{0}'>{1}{2}</div>", colorName, content, input);
+            return html;
+        }
+
+    }
+}
